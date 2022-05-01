96abc3e94d3600a7d2fbe668e17fbcdc81a00865
Yuu Funyu <mfunyu@student.42tokyo.jp>
2022-05-01 17:19:54 +0900

    add: 42mac_setup.sh

---
+++ 42mac_setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+#-------------------MenuBar----------------------#
+## display textinput icon
+defaults write com.apple.systemuiserver \
+  "NSStatusItem Visible com.apple.menuextra.textinput" 1
+defaults write com.apple.systemuiserver \
+  menuExtras -array-add "/System/Library/CoreServices/Menu Extras/TextInput.menu"
+## display volume icon
+defaults write com.apple.systemuiserver \
+  "NSStatusItem Visible com.apple.menuextra.volume" 1
+defaults write com.apple.systemuiserver \
+  menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Volume.menu"
+
+# reload MenuBar
+killall SystemUIServer
+
+#-------------------Dock----------------------#
+defaults write com.apple.dock orientation left
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock largesize -int 96
+defaults write com.apple.dock autohide -bool true
+
+## delete all icons
+defaults write com.apple.dock persistent-apps -array
+
+# for recovery
+# defaults delete com.apple.dock
+
+# reload Dock
+killall Dock
+
+#-------------------Others----------------------#
+## screen shot prefix
+defaults write com.apple.screencapture name "SS"
+
+# reload
+killall SystemUIServer
+
+
+
+## ban .DS_store
+defaults write com.apple.desktopservices DSDontWriteNetworkStores True
+
+# reload
+killall Finder
+
+
+#-------------------Install brew------------------#
+# install brew
+if [ ! -e $HOME/goinfre/.brew ]; then
+curl -fsSL https://raw.githubusercontent.com/mfunyu/config/main/42homebrew_install.sh | zsh
+fi
+
+# apply brew cmd
+source ~/.zshrc
+
+#-------------------Custom Dock------------------#
+brew install dockutil
+
+dockutil --add /Applications/System\ Preferences.app
+dockutil --add /Applications/Launchpad.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/iTerm.app
+dockutil --add /Applications/Visual\ Studio\ Code.app
+dockutil --add /Applications/Discord.app
+dockutil --add /Applications/Slack.app
+dockutil --add /Applications/Calendar.app
+
