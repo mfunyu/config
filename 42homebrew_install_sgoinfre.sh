b55544ad49827d0eaf792149278b9607f7c4c60a
mfunyu <mfunyu@student.42lyon.fr>
2024-06-17 14:29:49 +0200

    fix: for sgoinfre nfs4 session

--- 42homebrew_install_sgoinfre.sh
+++ 42homebrew_install_sgoinfre.sh
@@ -13,7 +13,7 @@ rm -rf $HOME/sgoinfre/.brew
 git clone --depth=1 https://github.com/Homebrew/brew $HOME/sgoinfre/.brew
 
 # Create .brewconfig script in home directory
-cat > $HOME/sgoinfre/.brewconfig.zsh <<EOL
+cat > $HOME/.brewconfig.zsh <<EOL
 # HOMEBREW CONFIG
 
 # Add brew to path
@@ -26,25 +26,34 @@ export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
 mkdir -p \$HOMEBREW_CACHE
 mkdir -p \$HOMEBREW_TEMP
 
-# If NFS session
-# Symlink Locks folder in /tmp
-if df -T autofs,nfs \$HOME > /dev/null 2>&1
+# sgoinfre is NFS session
+# Symlink Locks and Cellar folders in /tmp
+HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
+HOMEBREW_LOCKS_FOLDER=\$HOME/sgoinfre/.brew/var/homebrew
+HOMEBREW_CELLAR_TARGET=/tmp/\$USER/Homebrew/Cellar
+HOMEBREW_CELLAR_FOLDER=\$HOME/sgoinfre/.brew/Cellar
+
+mkdir -p \$HOMEBREW_LOCKS_TARGET
+mkdir -p \$HOMEBREW_LOCKS_FOLDER
+mkdir -p \$HOMEBREW_CELLAR_TARGET
+mkdir -p \$HOMEBREW_CELLAR_FOLDER
+
+# Symlink to Locks target folders
+# If not already a symlink
+if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
 then
-  HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
-  HOMEBREW_LOCKS_FOLDER=\$HOME/sgoinfre/.brew/var/homebrew
-
-  mkdir -p \$HOMEBREW_LOCKS_TARGET
-  mkdir -p \$HOMEBREW_LOCKS_FOLDER
-
-  # Symlink to Locks target folders
-  # If not already a symlink
-  if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
-  then
-     echo "Creating symlink for Locks folder"
-     rm -rf \$HOMEBREW_LOCKS_FOLDER
-     ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
-  fi
+   echo "Creating symlink for Locks folder"
+   rm -rf \$HOMEBREW_LOCKS_FOLDER
+   ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
 fi
+
+if ! [[ -L \$HOMEBREW_CELLAR_FOLDER && -d \$HOMEBREW_CELLAR_FOLDER ]]
+then
+   echo "Creating symlink for Cellar folder"
+   rm -rf \$HOMEBREW_CELLAR_FOLDER
+   ln -s \$HOMEBREW_CELLAR_TARGET \$HOMEBREW_CELLAR_FOLDER
+fi
+
 EOL
 
 # Add .brewconfig sourcing in your .zshrc if not already present
@@ -53,11 +62,11 @@ then
 cat >> $HOME/.zshrc <<EOL
 
 # Load Homebrew config script
-source \$HOME/sgoinfre/.brewconfig.zsh
+source \$HOME/.brewconfig.zsh
 EOL
 fi
 
-source $HOME/sgoinfre/.brewconfig.zsh
+source $HOME/.brewconfig.zsh
 rehash
 brew update
 
